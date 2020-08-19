e3c53f2c4f87cf2d2065dae4342d3b7163df8bdd
alsnwlsn <63220409+alsnwlsn@users.noreply.github.com>
2020-08-19 17:46:42 +0900

    8월19일

---
+++ 추상클래스 인터페이스와 클래스 사이.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    abstract class AbstractBase
+    {
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethodA()");
+        }
+        public void PublicMethodA()
+        {
+            Console.WriteLine("AbstractBase.PublicMethodA()"); 
+        }
+        public abstract void AbstractMethodA();
+    }
+    class Derived : AbstractBase
+    {
+    /*    public void PrivateMethodA()
+        {
+            Console.WriteLine("PrivateMethod()");
+        } */
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("Derived.AbstractMethodA()");
+            PrivateMethodA();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractBase obj = new Derived();
+            obj.AbstractMethodA();
+            obj.PublicMethodA();
+            // obj.PrivateMethodA(); private 라서안댐
+        }
+    }
+}
