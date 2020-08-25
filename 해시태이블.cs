8d4ce26b6c180d28a34721915105281e416b4ebc
alsnwlsn <63220409+alsnwlsn@users.noreply.github.com>
2020-08-25 12:23:48 +0900

    8월25일(6차시)

---
+++ 해시태이블.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht["book"] = "책";
+            ht["하나"] = "one";
+            ht["둘"] = "two";
+            ht["셋"] = "three";
+            ht["넷"] = "four";
+            ht["다섯"] = "five";
+
+            Console.WriteLine(ht["book"]);
+            Console.WriteLine(ht["하나"]);
+            Console.WriteLine(ht["둘"]);
+            Console.WriteLine(ht["셋"]);
+            Console.WriteLine(ht["넷"]);
+            Console.WriteLine(ht["다섯"]);
+        }
+    }
+}
