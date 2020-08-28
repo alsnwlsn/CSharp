3d11dd865e8123b9fecf4450ba09c366698421a8
alsnwlsn <63220409+alsnwlsn@users.noreply.github.com>
2020-08-28 13:48:01 +0900

    8월28일(8차시)

---
+++ 일반화스택.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < 5; i++)
+                stack.Push(i + 1);
+
+            while (stack.Count > 0)
+                Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ 일반화큐.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                queue.Enqueue(i + 1);
+            }
+            while (queue.Count > 0)
+                Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
