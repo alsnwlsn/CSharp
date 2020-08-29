70ffe5e4d062b5096c7ca5f37c808a178b4757b0
alsnwlsn <63220409+alsnwlsn@users.noreply.github.com>
2020-08-29 13:19:24 +0900

    8월29일(9차시)

---
+++ TryCatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"예외 발생 : {e.Message}");
+            }
+            /*catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"예외 발생 : {e.Message}");
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine($"예외 발생 : {e.Message}");
+            } */
+        }
+    }
+}
---
+++ 예외던지기.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void DoSometing(int arg)
+        {
+            if (arg < 10)
+                Console.WriteLine($"arg : {arg}");
+            else
+                throw new Exception("arg가 10보다 큽니다.");
+        }
+        static void Main(string[] args)
+        {
+            try
+            { 
+                for (int i = 1; i < 15; i += 2)
+                {
+                    DoSometing(i);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
