b91ecb130d42255d99f67d8773bd3b048cc6f180
alsnwlsn <63220409+alsnwlsn@users.noreply.github.com>
2020-09-03 14:51:59 +0900

    9월3일(14차시)

---
+++ 이진파일.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryWriter bw = new BinaryWriter(new FileStream("a.dat", FileMode.Create));
+            bw.Write(int.MaxValue);
+            bw.Write("good morning");
+            bw.Write(uint.MaxValue);
+            bw.Write("안녕하세요");
+            bw.Write(double.MaxValue);
+            bw.Close();
+
+            BinaryReader br = new BinaryReader(new FileStream("a.dat", FileMode.Open));
+            Console.WriteLine($"File size : {br.BaseStream.Length} bytes");
+            Console.WriteLine($"{br.ReadInt32()}");
+            Console.WriteLine($"{br.ReadString()}");
+            Console.WriteLine($"{br.ReadUInt32()}");
+            Console.WriteLine($"{br.ReadString()}");
+            Console.WriteLine($"{br.ReadDouble()}");
+            br.Close();
+        }
+    }
+}
---
+++ 텍스트파일.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(new FileStream("a.txt", FileMode.Create));
+            sw.Write(int.MaxValue);
+            sw.Write("good morning");
+            sw.Write(uint.MaxValue);
+            sw.Write("안녕하세요");
+            sw.Write(double.MaxValue);
+            sw.Close();
+
+            StreamReader sr = new StreamReader(new FileStream("a.txt", FileMode.Open));
+            Console.WriteLine($"File size : {sr.BaseStream.Length} bytes");
+            while (sr.EndOfStream == false)
+                Console.WriteLine(sr.ReadLine());
+            sr.Close();
+        }
+    }
+}
