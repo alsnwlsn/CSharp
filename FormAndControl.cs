5c762977e35c84e32135203800d61ab947a8d1a8
alsnwlsn <63220409+alsnwlsn@users.noreply.github.com>
2020-09-09 17:12:21 +0900

    9월9일

---
+++ FormAndControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp3
+{
+    class Program : Form
+    {
+        static void Main(string[] args)
+        {
+            Button button = new Button();
+            button.Text = "클릭";
+            button.Left = 100;
+            button.Top = 50;
+            int c = 0;
+            button.Click += (object sender, EventArgs e) =>
+            {
+                c++;
+                MessageBox.Show(c+"번 클릭");
+            };
+            Program form = new Program();
+            form.Text = "Form & Control";
+            form.Height = 150;
+            form.Controls.Add(button);
+            Application.Run(form);
+        }
+    }
+}
---
+++ FormSize.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp2
+{
+    class Program : Form
+    {
+        static void Main(string[] args)
+        {
+            Program form = new Program();
+            form.Width = 300;
+            form.Height = 200;
+            form.MouseDown += new MouseEventHandler(form_MouseDown);
+            Application.Run(form);
+        }
+        static void form_MouseDown(object sender, MouseEventArgs e)
+        {
+            Form form = (Form)sender;
+            int oldwidth = form.Width;
+            int oldheight = form.Height;
+            if(e.Button == MouseButtons.Left)
+            {
+                if(oldwidth < oldheight)
+                {
+                    form.Width = oldheight;
+                    form.Height = oldwidth;
+                }
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                if (oldheight < oldwidth)
+                {
+                    form.Width = oldheight;
+                    form.Height = oldwidth;                  
+                }
+            }
+            else if (e.Button == MouseButtons.Middle)
+            {
+                    if (oldwidth < oldheight)
+                    {
+                        form.Width = oldheight;
+                        form.Height = oldwidth;
+                    }
+                    else if (oldheight < oldwidth)
+                    {
+                        form.Width = oldheight;
+                        form.Height = oldwidth;
+                    }                
+            }
+            Console.WriteLine($"Width : {form.Width} Height : {form.Height}");
+        }
+    }
+}
