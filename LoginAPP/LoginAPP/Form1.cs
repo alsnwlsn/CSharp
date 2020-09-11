81f1804fd4684fc8ae130700176a8579e5bb18f0
alsnwlsn <63220409+alsnwlsn@users.noreply.github.com>
2020-09-11 16:01:21 +0900

    9월11일

---
+++ LoginAPP/LoginAPP/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LoginAPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\source\repos\LoginAPP\LoginAPP\login_db.mdf;Integrated Security=True");
+            string query = "SELECT * FROM Login WHERE username='"+textBox1.Text.Trim()+"'" + "and password ='"+textBox2.Text.Trim()+"'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtb1 = new DataTable();
+            sda.Fill(dtb1);
+            if(dtb1.Rows.Count == 1)
+            {
+                Form2 form2 = new Form2();
+                this.Hide();
+                form2.Show();
+            }
+            else
+            {
+                MessageBox.Show("아이디 또는 비번이 틀림");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
